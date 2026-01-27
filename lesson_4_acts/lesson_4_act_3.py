faf3c5962b97f942ae5978f090c379401ca3799c
Beta2012136 <bedipavit@gmail.com>
2026-01-27 18:57:09 +0000

    calculating percentage

---
+++ lesson_4_acts/lesson_4_act_3.py
@@ -0,0 +1,8 @@
+print("Enter 3 subjects ka mraks pls")
+science = int(input("Enter marks for science"))
+math = int(input("Enter marks for math"))
+english = int(input("Enter marks for english"))
+tot = science + math + english 
+per =(tot/300)*100
+print(per)
+90
