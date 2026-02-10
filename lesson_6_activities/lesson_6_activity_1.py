8711c31eddc11d41cfa33a82d3ba04194f921e01
Beta2012136 <bedipavit@gmail.com>
2026-02-10 18:12:40 +0000

    using logical operators, and or not

---
+++ lesson_6_activities/lesson_6_activity_1.py
@@ -0,0 +1,13 @@
+a = 13
+b = 6
+c = 0
+if a and b and c:
+    print("all these values are equal and have a bolean value of true")
+else:
+    print("at least 1 of these values has a bolean value of false")
+x = 18
+y = 19
+if x > 20 or y == 20:
+    print("20")
+else:
+    print(" x and y are both less than 20")
