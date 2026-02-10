4f6416aa10e4bd78e7aa639fa020d9483f0c9415
Beta2012136 <bedipavit@gmail.com>
2026-02-10 18:29:20 +0000

    not statments

---
+++ lesson_6_activities/lesson_6_activity_2.py
@@ -0,0 +1,12 @@
+a = 13
+b = 6
+c = 0
+if a != b :
+    print("a and b are not equal")
+else:
+    print("a and b are equal")
+
+if b != c:
+    print("b and c are not equal")
+else:  
+    print("b and c are equal")
